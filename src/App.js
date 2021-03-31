c920f9e29528fe09dd121894ad7b5bd77f05dfb8
Yuna Lee <y.duck10002@gmail.com>
2021-03-31 21:24:07 +0900

    Deleting To Dos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useReducer } from "react";
+import { v4 as uuid } from "uuid";
 
 const initialState = {
   toDos: [],
 };
 
 const ADD = "add";
-
+const DELETE = "delete";
 const reducer = (state, action) => {
   switch (action.type) {
     case ADD:
@@ -14,7 +15,11 @@ const reducer = (state, action) => {
        * 배열을 변경하는 것이 아닌, 새롭게 대체하는 것.
        * 기존 배열에 추가 및 삭제하는 것은 지양해야함
        */
-      return { toDos: [...state.toDos, { text: action.payload }] };
+      return { toDos: [...state.toDos, { text: action.payload, id: uuid() }] };
+    case DELETE:
+      return {
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
+      };
     default:
       return;
   }
@@ -27,6 +32,7 @@ function App() {
   const onSubmit = (e) => {
     e.preventDefault();
     dispatch({ type: ADD, payload: newToDo });
+    setNewToDo("");
   };
   const onChange = (e) => {
     const {
@@ -49,8 +55,15 @@ function App() {
 
       <ul>
         <h2>To Dos</h2>
-        {state.toDos.map((toDo, index) => (
-          <li key={index}>{toDo.text}</li>
+        {state.toDos.map((toDo) => (
+          <li key={toDo.id}>
+            <span>{toDo.text}</span>
+            <button
+              onClick={() => dispatch({ type: DELETE, payload: toDo.id })}
+            >
+              ❌
+            </button>
+          </li>
         ))}
       </ul>
     </>
